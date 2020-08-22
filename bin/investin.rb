5b021b92f7b2706165a765c00d8384bcbfeaae4f
Eric Promislow <epromislow@suse.com>
2020-08-21 21:48:06 -0700

    do proper command-line parsing

--- bin/investin.rb
+++ bin/investin.rb
@@ -4,6 +4,7 @@
 # emit a bunch of yaml
 
 require 'date'
+require 'optparse'
 require 'yaml'
 
 $: << File.expand_path("../lib", File.dirname($0))
@@ -14,19 +15,70 @@ require 'plugin_manager'
 require 'analyze'
 require 'spreadsheet'
 
-def usage(msg=nil)
-  $stderr.puts "Usage #{$0} yyyy-mm-dd inputDir"
-  $stderr.puts msg if msg
+options = {}
+optparse = OptionParser.new do |opts|
+  opts.banner = "Usage:  #{$0} [options]"
+
+  options[:date] = Date.today.to_s
+  opts.on('-d', '--date STRING', 'read info for date yyyy-mm-dd') do |date|
+    if /\A\d{4}-\d{2}-\d{2}\z/ !~ date
+      usage("Error: invalid date format: '#{date}'")
+    end
+    options[:date] = date
+  end
+
+  options[:inputDir] = File.expand_path("../data", File.dirname($0))
+  opts.on('-i', '--input-dir DIR', 'read csv files from DIR') { |dir| options[:inputDir] = dir }
+
+  options[:categories] = File.expand_path("../categories.yml", File.dirname($0))
+  opts.on('-c', '--categories FILE', 'read categories from FILE') { |path| options[:categories] = path }
+
+  options[:summary] = false
+  opts.on('--summary', 'emit a summary to stdout') { options[:summary] = true }
+
+  options[:spreadsheet] = nil
+  opts.on('-s', '--spreadsheet FILE', 'emit the spreadsheet to DIR/PATH') { |path| options[:spreadsheet] = path }
+
+  options[:forceOverwrite] = false
+  opts.on('-f', '--force-overwrite', 'overwrite existing spreadsheet') { options[:forceOverwrite] = true }
+
+  opts.on('-h', '--help') do
+    puts opts
+    exit 1
+  end
+end
+
+def usage(message)
+  $stderr.puts(message)
+  $stderr.puts `ruby "#{$0}" -h`
   exit 1
 end
 
-if ARGV.size < 2
-  usage("not enough args")
+begin
+  optparse.parse!
+rescue => ex
+  if ex.class.to_s.start_with?("OptionParser")
+    usage(ex.message)
+  end
 end
 
-date=ARGV[0]
-input_dir = ARGV[1]
+if ARGV.size > 0
+  usage "Unhandled args: #{ARGV}"
+end
+
+if options[:forceOverwrite] && options[:spreadsheet].nil?
+  usage("Need to specify -s/--spreadsheet when --force-overwrite is specified")
+end
+
+if options[:spreadsheet].nil? && !options[:summary]
+  options[:summary] = true
+end
+# pp options
+
+date=options[:date]
 dateObj = Date.parse(date)
+input_dir = options[:inputDir]
+categories_file = options[:categories]
 
 sources = []
 PluginManager.each do |analyzerName, analyzerClass|
@@ -39,8 +91,27 @@ PluginManager.each do |analyzerName, analyzerClass|
     entries: paths.map { |path| deep_symbolize_keys(analyzerClass.new.parse(path)) },
   }
 end
+if sources.size == 0
+  abort "No input for date #{date}"
+end
 
-analyzer = Analyze::Analyzer.new("categories.yml")
+analyzer = Analyze::Analyzer.new(categories_file)
 analyzer.process(sources)
-analyzer.print_summary
-Spreadsheet.new.create("tmp/full-summary-#{dateObj}.ods", analyzer)
+
+if options[:summary]
+  analyzer.print_summary
+end
+
+if (spreadsheet_location = options[:spreadsheet])
+  if File.directory?(spreadsheet_location)
+    path = File.join(spreadsheet_location, "full-summary-#{dateObj}.ods")
+  else
+    path = spreadsheet_location
+  end
+  if File.exist?(path) && !options[:forceOverwrite]
+    puts "File #{path} already exists. Run with -f to overwrite"
+    exit 0
+  end
+  Spreadsheet.new.create(path, analyzer)
+  system(%Q/open "#{path}"/)
+end
